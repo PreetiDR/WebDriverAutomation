3ee50ca99ec053479331b4ddef3554594b231f11
PreetiDR <preeti.ratnakar@dzeesolutions.com>
2017-12-20 13:29:31 -0700

    First Modification commit

--- src/test/java/PlanRecommendAutomation/Automation/LoginTest.java
+++ src/test/java/PlanRecommendAutomation/Automation/LoginTest.java
@@ -3,25 +3,29 @@ package PlanRecommendAutomation.Automation;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 
 public class LoginTest {
 	
+	
 	public static WebDriver driver;
+	
 	
 	@BeforeTest
 	public void setUp()
 	{
-				driver= new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver","/Users/ratnakar/Downloads/chromedriver");		
+		driver= new ChromeDriver();
 				
 	}
 	@Test
 	public void doLogin()
 	{
-		driver.get("http://planselect.dzeecloud.com/employeePlanSelection/#!/");
+		driver.get("https://www.google.com/");
 		System.out.println("hello");
 	}
 	
